7962f18321a82ab71dbe4feeac174cc5ed797a3b
Scott Mertz <scott@cyngn.com>
2015-11-10 11:15:43 -0800

    adb insecure by default
    Change-Id: Iec14c80bb43b2f58cfad36e05177f4fec3b1fd64

--- config/common.mk
+++ config/common.mk
@@ -42,7 +42,7 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 # Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
 endif
 
 # Copy over the changelog to the device
