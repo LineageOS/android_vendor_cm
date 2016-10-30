1ff7db18605935bab33266a132a203dee2a6d17c
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-30 09:24:38 +0800

    cm: config: Add initial board & product components
    Change-Id: Iaafd4865e233d18ad53b10dd4efea2d06d57d52f

---
+++ config/product/common/nfc.mk
@@ -0,0 +1,4 @@
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
