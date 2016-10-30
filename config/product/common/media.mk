68bd4db69d1d755d405f88d087fd9c02c89329b5
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-30 09:24:38 +0800

    cm: config: Add initial board & product components
    Change-Id: Iaafd4865e233d18ad53b10dd4efea2d06d57d52f

---
+++ config/product/common/media.mk
@@ -0,0 +1,8 @@
+# Media
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
