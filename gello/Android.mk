45481dbcf28198d275c3b094a942c73ffa363f4f
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-12-22 05:51:42 +0800

    [DO NOT MERGE] cm: Use a local prebuilt Gello APK
    Change-Id: If54a3dff4f558b862207a4bd6a8ad53730530406

--- gello/Android.mk
+++ gello/Android.mk
@@ -42,11 +42,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_BUILT_MODULE_STEM := package.apk
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 
-LOCAL_MAVEN_REPO := https://maven.lineageos.org/artifactory/gello_prebuilds
-LOCAL_MAVEN_GROUP := org.lineageos
-LOCAL_MAVEN_VERSION := 40
-LOCAL_MAVEN_ARTIFACT := gello
-LOCAL_MAVEN_PACKAGING := apk
+LOCAL_SRC_FILES := Gello.apk
 
-include $(BUILD_MAVEN_PREBUILT)
+include $(BUILD_PREBUILT)
 endif
