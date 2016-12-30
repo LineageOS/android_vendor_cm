3da97189386359228589f5f9f77ead2034c835f3
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-12-30 16:26:44 -0500

    http_prebuilt: Implement archive of artifacts
    * checksum will be downloaded and asserted to prevent a
        redownload every clean
    * Archives will exist (invisibly) at vendor/cm/prebuilt/archive
    
    Change-Id: I637e86cacd146d665044a119885900dc4660e22b
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- build/core/http_prebuilt.mk
+++ build/core/http_prebuilt.mk
@@ -21,13 +21,51 @@ ifeq ($(strip $(LOCAL_HTTP_FILENAME)),)
   $(error LOCAL_HTTP_FILENAME not defined.)
 endif
 
-LOCAL_PREBUILT_MODULE_FILE := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE),,COMMON)/$(LOCAL_HTTP_FILENAME)
+ifeq ($(strip $(LOCAL_HTTP_MD5SUM)),)
+  $(error LOCAL_HTTP_MD5SUM not defined.)
+endif
 
-$(LOCAL_PREBUILT_MODULE_FILE): filename := $(LOCAL_HTTP_FILENAME)
+LOCAL_PREBUILT_MODULE_ARCHIVE := vendor/cm/prebuilt/archive/$(LOCAL_MODULE)
+LOCAL_PREBUILT_MODULE_FILE    := $(LOCAL_PREBUILT_MODULE_ARCHIVE)/$(LOCAL_HTTP_FILENAME).tmp
+LOCAL_PREBUILT_MODULE_MD5SUM  := $(LOCAL_PREBUILT_MODULE_ARCHIVE)/$(LOCAL_HTTP_MD5SUM)
+
+OBJ_PREBUILT_MODULE_FILE := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE),,COMMON)/$(LOCAL_HTTP_FILENAME)
+
+$(LOCAL_PREBUILT_MODULE_FILE): tmpfile  := $(LOCAL_PREBUILT_MODULE_FILE)
+$(LOCAL_PREBUILT_MODULE_FILE): filename := $(strip $(subst .tmp, , $(tmpfile)))
+$(LOCAL_PREBUILT_MODULE_FILE): checksum := $(LOCAL_PREBUILT_MODULE_MD5SUM)
+$(LOCAL_PREBUILT_MODULE_FILE): tmpsum   := $(LOCAL_PREBUILT_MODULE_ARCHIVE)/tmp.md5sum
+
+define curl-checksum
+  @echo "Pulling comparison md5sum for $(LOCAL_HTTP_FILENAME)"
+  $(hide) ./vendor/cm/build/tasks/http_curl_prebuilt.sh $(LOCAL_HTTP_PATH)/$(LOCAL_HTTP_MD5SUM) $(tmpsum)
+endef
+
+define audit-checksum
+  @echo "Downloading: $(LOCAL_HTTP_FILENAME) (version $(LOCAL_HTTP_FILE_VERSION))" -> $(tmpfile);
+  $(hide) if [ ! -f $(filename) ]; then \
+            ./vendor/cm/build/tasks/http_curl_prebuilt.sh $(LOCAL_HTTP_PATH)/$(LOCAL_HTTP_FILENAME) $(tmpfile); \
+            mv $(tmpfile) $(filename); \
+          else \
+            if [ $(shell echo $(md5sum $(filename))) != $(shell cat $(tmpsum) | cut -d ' ' -f1) ]; then \
+              rm -rf $(filename); \
+              ./vendor/cm/build/tasks/http_curl_prebuilt.sh $(LOCAL_HTTP_PATH)/$(LOCAL_HTTP_FILENAME) $(tmpfile); \
+              mv $(tmpfile) $(filename); \
+            fi; \
+          fi; \
+          rm $(tmpsum);
+endef
+
+define cleanup
+  @echo "Copying: $(LOCAL_HTTP_FILENAME) -> $(dir $(OBJ_PREBUILT_MODULE_FILE))"
+  $(hide) mkdir -p $(OBJ_PREBUILT_MODULE_FILE)
+  $(hide) cp $(filename) $(OBJ_PREBUILT_MODULE_FILE)
+endef
 
 $(LOCAL_PREBUILT_MODULE_FILE):
-	$(hide) curl -L $(LOCAL_HTTP_PATH) --create-dirs -o $(dir $@)/$(filename) --compressed -H "Accept-Encoding: gzip,deflate,sdch"
-	@echo "Download: $@"
+	$(call curl-checksum)
+	$(call audit-checksum)
+	$(call cleanup)
 
 include $(BUILD_PREBUILT)
 
