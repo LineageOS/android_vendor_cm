e8b36076a9a658404a3817cf3054a6fcb3113853
Gabriele M <moto.falcon.git@gmail.com>
2017-01-06 14:31:04 +0100

    extract_utils: Update header of autogenerated files
    Add/update the LineageOS copyright line without removing other
    copyrights.
    
    Change-Id: I3647c0565412b1aa7dd6bf90e49ddb17473ad428

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -438,33 +438,26 @@ function write_product_packages() {
 # be executed first!
 #
 function write_header() {
-    if [ -f $1 ]; then
-        rm $1
-    fi
-
     YEAR=$(date +"%Y")
 
     [ "$COMMON" -eq 1 ] && local DEVICE="$DEVICE_COMMON"
 
-    NUM_REGEX='^[0-9]+$'
-    if [[ $INITIAL_COPYRIGHT_YEAR =~ $NUM_REGEX ]] && [ $INITIAL_COPYRIGHT_YEAR -le $YEAR ]; then
-        if [ $INITIAL_COPYRIGHT_YEAR -lt 2016 ]; then
-            printf "# Copyright (C) $INITIAL_COPYRIGHT_YEAR-2016 The CyanogenMod Project\n" > $1
-        elif [ $INITIAL_COPYRIGHT_YEAR -eq 2016 ]; then
-            printf "# Copyright (C) 2016 The CyanogenMod Project\n" > $1
-        fi
-        if [ $YEAR -eq 2017 ]; then
-            printf "# Copyright (C) 2017 The LineageOS Project\n" >> $1
-        elif [ $INITIAL_COPYRIGHT_YEAR -eq $YEAR ]; then
-            printf "# Copyright (C) $YEAR The LineageOS Project\n" >> $1
-        elif [ $INITIAL_COPYRIGHT_YEAR -le 2017 ]; then
-            printf "# Copyright (C) 2017-$YEAR The LineageOS Project\n" >> $1
+    local EXISTING_COPYRIGHTS=`cat $1 2> /dev/null | grep '^#.\+(C) [0-9]\+.\+$'`
+    if [ -n "$EXISTING_COPYRIGHTS" ]; then
+        local LINEAGEOS_YEAR=`echo $EXISTING_COPYRIGHTS | grep LineageOS | sed -n 's:.\+ \([0-9]\+\).\+:\1:p'`
+        if [ -n "$LINEAGEOS_YEAR" ]; then
+            if [ $LINEAGEOS_YEAR -ne $YEAR ]; then
+                YEAR="$LINEAGEOS_YEAR-$YEAR"
+            fi
+            COPYRIGHTS=`echo "$EXISTING_COPYRIGHTS" | sed "s:\(.\+ (C)\) .\+ \(The LineageOS Project\):\1 $YEAR \2:g"`
         else
-            printf "# Copyright (C) $INITIAL_COPYRIGHT_YEAR-$YEAR The LineageOS Project\n" >> $1
+            COPYRIGHTS="# Copyright (C) $YEAR The LineageOS Project"
+            COPYRIGHTS=`printf "$EXISTING_COPYRIGHTS\n$COPYRIGHTS"`
         fi
     else
-        printf "# Copyright (C) $YEAR The LineageOS Project\n" > $1
+        COPYRIGHTS="# Copyright (C) $YEAR The LineageOS Project"
     fi
+    echo "$COPYRIGHTS" > $1
 
     cat << EOF >> $1
 #
