82880f032f0dbe9cc22a9cfa1fa1484fd87ea0bd
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-16 19:26:50 -0600

    vendor: add modified backuptools and lineage_postinstall script for A/B OTA
    * A/B OTA devices wont run backuptools in recovery (mainly because they
      don't go in to recovery to do an OTA). In these cases let's use a
      modified version to backup/restore from within android upon postinstall.
    
    Change-Id: I51511870634dd1ec5388adafddb446f95cc5a950

---
+++ prebuilt/common/bin/lineage_postinstall.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+#
+# LineageOS A/B OTA Postinstall Script
+#
+
+# Run otapreopt_script, the default AOSP postinstall script
+/system/bin/otapreopt_script "$@"
+
+# Backup and restore using backuptool_ab
+/system/bin/backuptool_ab.sh backup
+/system/bin/backuptool_ab.sh restore
+
+exit 0
