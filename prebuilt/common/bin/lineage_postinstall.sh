ecf21a46cfe97784cf6e643105eea2e6529673c5
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-16 19:26:50 -0600

    vendor: add custom backuptools and postinstall script for A/B OTAs
    * A/B OTA devices wont run backuptools in recovery (because they don't
      go in to recovery to do an OTA). In these cases let's use a modified
      version to backup/restore from within android upon postinstall.
    
    * Add lineage_postinstall.sh (which can be overridden in the device tree
      as the postinstall script) to call otapreopt_script (AOSP's default
      postinstall script) and then perform backup tool operations.
    
    * We need to remount the /postinstall directory because update_engine
      mounts it with the postinstall_file context and we cannot chcon
      because of that.
    
    Change-Id: I51511870634dd1ec5388adafddb446f95cc5a950

---
+++ prebuilt/common/bin/lineage_postinstall.sh
@@ -0,0 +1,45 @@
+#!/system/bin/sh
+#
+# LineageOS A/B OTA Postinstall Script
+#
+
+# Run otapreopt_script, the default AOSP postinstall script
+/postinstall/system/bin/otapreopt_script "$@"
+
+# Sleep for a few seconds to make sure /postinstall is no longer in use
+sleep 3
+
+# Unmount the context-mounted /postinstall
+umount /postinstall
+
+# Let's loop a bit just to ensure this got unmounted...hack, shitty, but effective
+for i in `seq 1 10`; do
+  if mount | grep /postinstall; then
+    echo "/postinstall still mounted. Attempting unmount in 3 seconds..."
+    sleep 3
+    unmount /postinstall
+  else
+    echo "/postinstall successfully unmounted. Continuing with backuptool..."
+    break
+  fi
+done
+
+# Get slot for new install
+TARGET_SLOT="$1"
+if [ "$TARGET_SLOT" = "0" ] ; then
+  TARGET_SLOT_SUFFIX="_a"
+elif [ "$TARGET_SLOT" = "1" ] ; then
+  TARGET_SLOT_SUFFIX="_b"
+else
+  echo "Unknown target slot $TARGET_SLOT"
+  exit 1
+fi
+
+# Mount without a context and perform backuptool operations
+mount /dev/block/bootdevice/by-name/system$TARGET_SLOT_SUFFIX /postinstall
+/postinstall/system/bin/backuptool_ab.sh backup
+/postinstall/system/bin/backuptool_ab.sh restore
+
+sync
+
+exit 0
