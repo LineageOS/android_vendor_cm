e2331047ba29b8534e52b8be794e436841ef06ea
Asher Simonds <dayanhammer@gmail.com>
2017-01-16 21:52:24 -0600

    vendor: lineage boot animation
    Change-Id: Iff2087eb97aee5e586eecf3898f0066d99bc885c

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -6,27 +6,30 @@ HALF_RES="$3"
 OUT="$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION"
 
 if [ "$HEIGHT" -lt "$WIDTH" ]; then
-    SIZE="$HEIGHT"
+    IMAGEWIDTH="$HEIGHT"
 else
-    SIZE="$WIDTH"
+    IMAGEWIDTH="$WIDTH"
 fi
+
+IMAGESCALEWIDTH="$IMAGEWIDTH"
+IMAGESCALEHEIGHT=$(( $IMAGESCALEWIDTH / 3 ))
 
 if [ "$HALF_RES" = "true" ]; then
-    IMAGESIZE=$(expr $SIZE / 2)
-else
-    IMAGESIZE="$SIZE"
+    IMAGEWIDTH=$(( $IMAGEWIDTH / 2 ))
 fi
 
-RESOLUTION=""$IMAGESIZE"x"$IMAGESIZE""
+IMAGEHEIGHT=$(( $IMAGEWIDTH / 3 ))
 
-for part_cnt in 0 1 2
+RESOLUTION=""$IMAGEWIDTH"x"$IMAGEHEIGHT""
+
+for part_cnt in {0..2}
 do
-    mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part$part_cnt
+    mkdir -p $OUT/bootanimation/part$part_cnt
 done
 tar xfp "vendor/cm/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' \"png8:$OUT/bootanimation/\$TAR_FILENAME\""
 
 # Create desc.txt
-echo "$SIZE" "$SIZE" 30 > "$OUT/bootanimation/desc.txt"
+echo "$IMAGESCALEWIDTH $IMAGESCALEHEIGHT" 30 > "$OUT/bootanimation/desc.txt"
 cat "vendor/cm/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.txt"
 
 # Create bootanimation.zip
