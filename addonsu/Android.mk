c237620b106e2756b82516e675fd411b59a23021
Gabriele M <moto.falcon.git@gmail.com>
2017-01-21 18:22:09 +0100

    Add target to make a flashable su addon
    Change-Id: I5b2fe67f98f2474b923c074dc6025b47c6db2ae0

---
+++ addonsu/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mount-system
+LOCAL_C_INCLUDES := system/core/fs_mgr/include
+LOCAL_SRC_FILES := mount-system.cpp
+LOCAL_STATIC_LIBRARIES := libfs_mgr
+LOCAL_WHOLE_STATIC_LIBRARIES += libcutils
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
