99b33f5602ee403bce79f4fb100284971212780c
Gabriele M <moto.falcon.git@gmail.com>
2017-01-22 22:50:02 +0100

    Add target to make a flashable su addon
    Run 'make addonsu' to make a flashable zip to install the addon and
    'make addonsu-remove' for a flashable zip that removes it.
    
    Change-Id: I5b2fe67f98f2474b923c074dc6025b47c6db2ae0

---
+++ addonsu/51-addonsu.sh
@@ -0,0 +1,40 @@
+#!/sbin/sh
+
+. /tmp/backuptool.functions
+
+# etc/addonsu should be enough
+list_files() {
+cat <<EOF
+etc/addonsu
+etc/init/superuser.rc
+bin/su
+xbin/su
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/"$FILE"
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/"$FILE" "$R"
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    # Stub
+  ;;
+esac
---
+++ build/tasks/addonsu.mk
@@ -0,0 +1,44 @@
+ADDONSU_PREBUILTS_PATH := vendor/cm/addonsu/
+
+ADDONSU_INSTALL_OUT := $(PRODUCT_OUT)/addonsu-install/
+ADDONSU_INSTALL_TARGET := $(PRODUCT_OUT)/addonsu-$(TARGET_ARCH).zip
+
+SU_BINARY := $(ALL_MODULES.su.BUILT)
+SU_RC := $(TARGET_OUT_ETC)/init/superuser.rc
+UPDATER_BINARY := $(ALL_MODULES.updater.BUILT)
+
+$(ADDONSU_INSTALL_TARGET): $(UPDATER_BINARY) $(SU_BINARY) $(SU_RC)
+	$(hide) rm -rf $@ $(ADDONSU_INSTALL_OUT)
+	$(hide) mkdir -p $(ADDONSU_INSTALL_OUT)/META-INF/com/google/android/
+	$(hide) mkdir -p $(ADDONSU_INSTALL_OUT)/system/xbin
+	$(hide) mkdir -p $(ADDONSU_INSTALL_OUT)/system/etc
+	$(hide) mkdir -p $(ADDONSU_INSTALL_OUT)/system/addon.d
+	$(hide) mkdir -p $(ADDONSU_INSTALL_OUT)/system/etc/init
+	$(hide) touch $(ADDONSU_INSTALL_OUT)/system/etc/addonsu
+	$(hide) cp $(SU_BINARY) $(ADDONSU_INSTALL_OUT)/system/xbin/
+	$(hide) cp $(SU_RC) $(ADDONSU_INSTALL_OUT)/system/etc/init/
+	$(hide) cp $(UPDATER_BINARY) $(ADDONSU_INSTALL_OUT)/META-INF/com/google/android/update-binary
+	$(hide) cp $(ADDONSU_PREBUILTS_PATH)/51-addonsu.sh $(ADDONSU_INSTALL_OUT)/system/addon.d/
+	$(hide) cp $(ADDONSU_PREBUILTS_PATH)/mount-system.sh $(ADDONSU_INSTALL_OUT)/
+	$(hide) cp $(ADDONSU_PREBUILTS_PATH)/updater-script-install $(ADDONSU_INSTALL_OUT)/META-INF/com/google/android/updater-script
+	$(hide) (cd $(ADDONSU_INSTALL_OUT) && zip -qr $@ *)
+
+.PHONY: addonsu
+addonsu: $(ADDONSU_INSTALL_TARGET)
+	@echo "Done: $(ADDONSU_INSTALL_TARGET)"
+
+
+ADDONSU_REMOVE_OUT := $(PRODUCT_OUT)/addonsu-remove/
+ADDONSU_REMOVE_TARGET := $(PRODUCT_OUT)/addonsu-remove-$(TARGET_ARCH).zip
+
+$(ADDONSU_REMOVE_TARGET): $(UPDATER_BINARY)
+	$(hide) rm -rf $@ $(ADDONSU_REMOVE_OUT)
+	$(hide) mkdir -p $(ADDONSU_REMOVE_OUT)/META-INF/com/google/android/
+	$(hide) cp $(UPDATER_BINARY) $(ADDONSU_REMOVE_OUT)/META-INF/com/google/android/update-binary
+	$(hide) cp $(ADDONSU_PREBUILTS_PATH)/mount-system.sh $(ADDONSU_REMOVE_OUT)/
+	$(hide) cp $(ADDONSU_PREBUILTS_PATH)/updater-script-remove $(ADDONSU_REMOVE_OUT)/META-INF/com/google/android/updater-script
+	$(hide) (cd $(ADDONSU_REMOVE_OUT) && zip -qr $@ *)
+
+.PHONY: addonsu-remove
+addonsu-remove: $(ADDONSU_REMOVE_TARGET)
+	@echo "Done: $(ADDONSU_REMOVE_TARGET)"
