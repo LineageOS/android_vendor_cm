f651a51f34d6882f8fa9172469f3c02cef87b0ae
ShadowReaper1 <androgv@gmail.com>
2017-04-08 22:41:47 +0530

    vendor: add bootanimation

--- bootanimation/Android.mk
+++ bootanimation/Android.mk
@@ -16,7 +16,7 @@
 #
 
 define build-bootanimation
-    sh vendor/cm/bootanimation/generate-bootanimation.sh \
+    sh vendor/aim/bootanimation/generate-bootanimation.sh \
     $(TARGET_SCREEN_WIDTH) \
     $(TARGET_SCREEN_HEIGHT) \
     $(TARGET_BOOTANIMATION_HALF_RES)
