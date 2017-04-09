bc8e15d03b1f1caf2fb57398d96702a24a1d85da
Paul Keith <javelinanddart@bestas.gr>
2017-04-09 09:52:30 -0500

    config: partner_gms: Update gms integration
    Change-Id: I4b2ccfa9cbde17a30e3fc6ecbd3527101cc94c79
    Signed-off-by: Paul Keith <javelinanddart@bestas.gr>

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,3 +1,5 @@
 ifeq ($(WITH_GMS),true)
-$(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
+ifneq ($(TARGET_ARCH),)
+$(call inherit-product-if-exists, vendor/gapps/$(TARGET_ARCH)/$(TARGET_ARCH)-vendor.mk)
+endif
 endif
