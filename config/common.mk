457266dacad472845c6475e29ee26878eb5e01fa
Anand Ramachandran <anand.nv1998@gmail.com>
2017-05-17 16:13:41 +0530

    Change changelog name

--- config/common.mk
+++ config/common.mk
@@ -33,7 +33,7 @@ endif
 
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
-    vendor/aim/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
+    vendor/aim/CHANGELOG.mkdn:system/etc/AIMLOG.txt
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
