816757e3d481bd2b3d59d589facb68006424765c
Akhil Narang <akhilnarang.1999@gmail.com>
2017-05-24 18:59:23 +0530

    Add Root method picker script

---
+++ extra/root.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Root method chooser for the Resurrection Remix ROM
+# Specify colors utilized in the terminal
+    red=$(tput setaf 1)             #  red
+    grn=$(tput setaf 2)             #  green
+    ylw=$(tput setaf 3)             #  yellow
+    blu=$(tput setaf 4)             #  blue
+    ppl=$(tput setaf 5)             #  purple
+    cya=$(tput setaf 6)             #  cyan
+    txtbld=$(tput bold)             #  Bold
+    bldred=${txtbld}$(tput setaf 1) #  red
+    bldgrn=${txtbld}$(tput setaf 2) #  green
+    bldylw=${txtbld}$(tput setaf 3) #  yellow
+    bldblu=${txtbld}$(tput setaf 4) #  blue
+    bldppl=${txtbld}$(tput setaf 5) #  purple
+    bldcya=${txtbld}$(tput setaf 6) #  cyan
+    txtrst=$(tput sgr0)             #  Reset
+    rev=$(tput rev)                 #  Reverse color
+    pplrev=${rev}$(tput setaf 5)
+    cyarev=${rev}$(tput setaf 6)
+    ylwrev=${rev}$(tput setaf 3)
+    blurev=${rev}$(tput setaf 4)
+    normal='tput sgr0'
+
+if [ -z "${WITH_MAGISK}" ]; then
+echo -e ${ylw}"\n\n ▼ Which root method do you want to use?\n"${txtrst}
+echo -e "";
+echo -e ${blu}" 〉 0- Magisk "${txtrst}${red}"    ︱ Magisk Root Method"${txtrst}
+echo -e "";
+echo -e ${blu}" 〉 1 or any other input- Without magisk "${txtrst}${red}"  ︱ Without magisk but with stock su binary [default]"${txtrst}
+echo -e "";
+echo -e ${blu}" 🕑  30/sec Time-out"${txtrst}${red}"︱ Default option"${txtrst}
+echo -e "";
+echo -e ${cya}" ▼ Pick a Method"${txtrst}
+echo -e "";
+$normal
+read -t 30 askvariant
+sleep 1
+if [ "$askvariant" == "0" ];
+then
+echo -e "";
+echo -e ${blu}" ▪ WITH MAGISK ROOT METHOD! "${txtrst}
+export WITH_MAGISK="true";
+echo -e "";
+echo -e "";
+$normal
+sleep 1
+else
+echo -e "";
+echo -e ${blu}" ▪ WITHOUT MAGISK "${txtrst}
+export WITH_MAGISK="false";
+echo -e "";
+echo -e "";
+$normal
+sleep 1
+fi
+fi
