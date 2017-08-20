61a84904fac3ebc82ebd648d97c362239567892c
Michael Bestas <mkbestas@lineageos.org>
2017-08-20 16:07:57 +0300

    cm: Remove LiveLockScreenService
    Change-Id: I8148e8a6c49f940c23b89e4f5fe697808dd08e93

--- config/cmsdk_common.mk
+++ config/cmsdk_common.mk
@@ -3,7 +3,6 @@ PRODUCT_COPY_FILES += \
     vendor/cm/config/permissions/org.cyanogenmod.appsuggest.xml:system/etc/permissions/org.cyanogenmod.appsuggest.xml \
     vendor/cm/config/permissions/org.cyanogenmod.audio.xml:system/etc/permissions/org.cyanogenmod.audio.xml \
     vendor/cm/config/permissions/org.cyanogenmod.livedisplay.xml:system/etc/permissions/org.cyanogenmod.livedisplay.xml \
-    vendor/cm/config/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml \
     vendor/cm/config/permissions/org.cyanogenmod.partner.xml:system/etc/permissions/org.cyanogenmod.partner.xml \
     vendor/cm/config/permissions/org.cyanogenmod.performance.xml:system/etc/permissions/org.cyanogenmod.performance.xml \
     vendor/cm/config/permissions/org.cyanogenmod.profiles.xml:system/etc/permissions/org.cyanogenmod.profiles.xml \
--- config/common.mk
+++ config/common.mk
@@ -127,7 +127,6 @@ PRODUCT_PACKAGES += \
     Eleven \
     ExactCalculator \
     Jelly \
-    LiveLockScreenService \
     LockClock \
     Trebuchet \
     Updater \
