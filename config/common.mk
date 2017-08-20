fdb2a33d73b9171e49fe8108a83571d8ebc5406a
Michael Bestas <mkbestas@lineageos.org>
2017-08-20 15:42:23 +0300

    cm: Remove theme engine leftovers
    Change-Id: Ife3fa6d6bfb5994fa120a959e6c5ba1faf6b2416

--- config/common.mk
+++ config/common.mk
@@ -84,9 +84,6 @@ PRODUCT_COPY_FILES += \
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
 
-# Theme engine
-include vendor/cm/config/themes_common.mk
-
 ifneq ($(TARGET_DISABLE_CMSDK), true)
 # CMSDK
 include vendor/cm/config/cmsdk_common.mk
