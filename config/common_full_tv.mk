a7b9841ac5f224e8b73e29d652b8e182b58517b1
Abhisek Devkota <ciwrl@lineageos.org>
2017-11-04 20:17:24 -0700

    [ATV] Add whitelist permission for SetupWraith
    Change-Id: If1915a0b72e05bdd9d5db49d131cf2abc5beb09c

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -4,3 +4,7 @@ $(call inherit-product, vendor/cm/config/common_full.mk)
 PRODUCT_PACKAGES += TvSettings
 
 DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/tv
+
+# SetupWraith needs WRITE_SECURE_SETTINGS as its not platform signed
+PRODUCT_COPY_FILES += \
+    vendor/cm/config/permissions/com.google.android.tungsten.setupwraith.xml:system/etc/permissions/com.google.android.tungsten.setupwraith.xml
--- config/common_mini_tv.mk
+++ config/common_mini_tv.mk
@@ -1,2 +1,6 @@
 # Inherit common CM stuff
 $(call inherit-product, vendor/cm/config/common_mini.mk)
+
+# SetupWraith needs WRITE_SECURE_SETTINGS as its not platform signed
+PRODUCT_COPY_FILES += \
+    vendor/cm/config/permissions/com.google.android.tungsten.setupwraith.xml:system/etc/permissions/com.google.android.tungsten.setupwraith.xml
