751d9fcb437916b5549c7aee92c5221607f79f5a
Michael Bestas <mkbestas@lineageos.org>
2018-03-21 18:29:45 +0200

    cm: Set TARGET_CRYPTFS_HW_PATH if HW crypto is enabled
    * So TWRP can use it
    
    Change-Id: Ibc1d9d943d6d4e4e5248f623cd06782335f8ae98

--- config/BoardConfigCM.mk
+++ config/BoardConfigCM.mk
@@ -10,3 +10,8 @@ else
   ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.device.cache_dir=/cache
 endif
+
+# QCOM HW crypto
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+endif
