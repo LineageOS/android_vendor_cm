339b19e351033d66017df06a6ad8cd3887d5c2bb
Vladimir Oltean <olteanv@gmail.com>
2018-04-09 21:56:32 +0300

    tasks: add "patch" and "unpatch" targets to support board-provided patches
    * Specific devices might need some core behavior in the AOSP framework
      to be slightly customized, and existing mechanisms (such as overlays)
      might not be flexible enough for the scenario.
    * With this patch, every device may specify in its device.mk a
      BOARD_PATCH_DIRS variable that contains a whitespace-delimited list of
      directories (relative to the AOSP root directory - generally in the
      form of "$(LOCAL_PATH)/patches").
    * Each file with a "*.patch" extension in the BOARD_PATCH_DIRS is applied
      in alphabetic order by running "make patch". Paths to patched files
      should also be specified relative to the AOSP root directory - it is
      thus possible that a single .patch file touches multiple git repositories.
    * The "make patch" command will leave the changes uncommitted and outside
      the git staging area. By running "make unpatch", all git repositories
      touched by the patches in $(BOARD_PATCH_DIRS) are brought back to a
      clean state (again without changing their HEAD).
    * The patches will remain applied until "make unpatch", or until
      "repo sync --force-sync" etc.
    
    Change-Id: I5ca480b00a8b51d1c6ac273314ba003a2f98d0f8
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

---
+++ build/tasks/patches.mk
@@ -0,0 +1,65 @@
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq ($(filter UNOFFICIAL EXPERIMENTAL, $(CM_BUILDTYPE)),)
+  # Only check the presence of BOARD_PATCH_DIRS on build types
+  # outside the filter above
+  ifneq ($(BOARD_PATCH_DIRS),)
+    $(warning ***************************************************************)
+    $(warning * Board-provided patches are only supported for)
+    $(warning * UNOFFICIAL and EXPERIMENTAL build types.)
+    $(warning * You are trying to build as $(CM_BUILDTYPE)!)
+    $(warning * Please consider removing the BOARD_PATCH_DIRS definition)
+    $(warning * from your device repository makefiles.)
+    $(warning ***************************************************************)
+    $(error BOARD_PATCH_DIRS unsupported for $(CM_BUILDTYPE) build)
+  endif
+endif
+
+.PHONY: patch
+patch: unpatch
+	$(hide)                                                               \
+	for patch_dir in $(BOARD_PATCH_DIRS); do                              \
+	  for patch in $$(find $${patch_dir} -type f -name "*.patch"); do     \
+	    echo "Applying patch $${patch}...";                               \
+	    patch -p1 --reject-file=- --batch --no-backup-if-mismatch         \
+	              --forward < $${patch} || {                              \
+	      rc=$$?;                                                         \
+	      echo "Patch $${patch} failed with code $${rc}, exiting...";     \
+	      exit $${rc};                                                    \
+	    };                                                                \
+	  done;                                                               \
+	done;
+
+.PHONY: unpatch
+unpatch:
+	$(hide)                                                               \
+	git_repos="";                                                         \
+	for patch_dir in $(BOARD_PATCH_DIRS); do                              \
+	  for patch in $$(find $${patch_dir} -type f -name "*.patch"); do     \
+	    touched_files="$$(lsdiff --strip 1 $${patch})";                   \
+	    for file in $${touched_files}; do                                 \
+	      dir="$$(dirname $${file})";                                     \
+	      repo_dir="$$(cd $${dir} && git rev-parse --show-toplevel)";     \
+	      git_repos="$${git_repos}:$${repo_dir}";                         \
+	    done;                                                             \
+	  done;                                                               \
+	done;                                                                 \
+	git_repos="$$(echo $${git_repos} | tr ':' '\n' | sort -u)";           \
+	for git_repo in $${git_repos}; do                                     \
+	  echo "Cleaning git repo $${git_repo}...";                           \
+	  (cd $${git_repo} && git checkout -f && git clean -fd);              \
+	done;
+
+
