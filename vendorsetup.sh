0e729bea0a9c7db8b310bb25f6af0a2fe9a94e32
Andreas Blaesius <andi@unlegacy-android.org>
2018-04-14 17:27:02 +0200

    [HACK] Patch external/svox
    The svox repo can't be forked due to the following:
    https://github.com/github/dmca/blob/master/2014/2014-12-22-Cambridge-Mobile.md
    
    On Android 7 we use cee78199bbfae81f54a40671db47096f5f32cda as
    revision ( https://android.googlesource.com/platform/external/svox/+/cee78199bbfae81f54a40671db47096f5f32cda )
    which does not work on Android 6 and below.
    
    Create the bug-id (bug_69177126) as empty file and check if it exist
    to decide if external/svox needs to be patched or not.
    For later use (Android 4?) and use in vendor/extra
    also check for the PLATFORM_VERSION.
    
    Change-Id: Id312672ec1a93d4e52b0f81ec8134419ca70ead4
    Signed-off-by: Andreas Blaesius <andi@unlegacy-android.org>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,16 @@ for combo in $(curl -s https://raw.githubusercontent.com/LineageOS/hudson/master
 do
     add_lunch_combo $combo
 done
+
+if grep -q "PLATFORM_VERSION := 6.0.1" build/core/version_defaults.mk
+  then
+  cd external/svox
+  if [ -e "bug_69177126" ]; then
+    echo 'external/svox patched already';
+  else
+    git am ../../vendor/cm/patch/external/svox/0001-SVOX-Properly-initialize-buffers.patch || git am --abort
+  fi
+else
+  echo 'This is not Android 6, patching external/svox not possible';
+fi
+croot
