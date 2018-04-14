51d8afcc0a7b68b7e2ec78ae20889963a8a6a0b6
Andreas Blaesius <andi@unlegacy-android.org>
2018-04-14 17:27:02 +0200

    [HACK] Patch external/svox
    The svox repo can't be forked due to the following:
    https://github.com/github/dmca/blob/master/2014/2014-12-22-Cambridge-Mobile.md
    
    On Android 7 we use cee78199bbfae81f54a40671db47096f5f32cda as
    revision ( https://android.googlesource.com/platform/external/svox/+/cee78199bbfae81f54a40671db47096f5f32cda )
    which does not work on Android 6 and below.
    
    Greps through the git log to determine if the specific Bug-ID
    has been fixed already. If it isn't, it applies the patch using
    git am.
    
    Change-Id: Id312672ec1a93d4e52b0f81ec8134419ca70ead4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,26 @@ for combo in $(curl -s https://raw.githubusercontent.com/LineageOS/hudson/master
 do
     add_lunch_combo $combo
 done
+
+while read -r file; do
+    project="$(dirname ${file} | sed 's|vendor/cm/patch/||g')"
+    bugid="$(grep '^Bug: ' ${file})"
+    while read -r line; do
+        if [ "$(git -C ${project} log --grep "${line}")" ]; then
+            continue
+        fi
+
+        git -C "${project}" am -q "$(pwd)/${file}"
+
+        if [ $? = 0 ]; then
+            echo "${project}: ${file} succeeded to apply"
+        else
+            git -C "${project}" am --abort
+            echo "${project}: ${file} failed to apply"
+        fi
+
+        break
+    done <<< "${bugid}"
+done <<< "$(find vendor/cm/patch -type f)"
+unset project
+unset bugid
