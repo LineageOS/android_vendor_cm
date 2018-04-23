LineageOS/android_vendor_cm

commit 72765e4cc950d90abb730bc62e3f5f9eeba5aa4e

write_makefiles: fix paths in BUILD_PREBUILT with destination
 * proprietary-files.txt entries such as
   "-app/TimeService/TimeService.apk:priv-app/TimeService/TimeService.apk" should
   generate a "LOCAL_SRC_FILES := proprietary/priv-app/TimeService/TimeService.apk"
   in the Makefile definition.
 * However, currently, the prefix_match function is being called on the whole
   PRODUCT_PACKAGES_LIST entry (whole line, including ":"), and therefore,
   TimeService.apk would be included in the APPS list instead of PRIV_APPS.
 * Furthermore, because of the incorrect prefix_match, the generated
   LOCAL_SRC_FILES is "proprietary/app/priv-app/TimeService/TimeService.apk",
   which breaks the build because there is no file at that wrong path.
 * The fix is to make the extract function match up with write_product_packages
   by applying the target_file function on all BUILD_PREBUILT source files.

Change-Id: Ib4859b8854db0f2142bb3f28cce8dd25f7141732
Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

build/tools/extract_utils.sh
@@ -148,7 +148,8 @@ function target_args() {
 #
 function prefix_match() {
     local PREFIX="$1"
-    for FILE in "${PRODUCT_PACKAGES_LIST[@]}"; do
+    for LINE in "${PRODUCT_PACKAGES_LIST[@]}"; do
+        local FILE=$(target_file "$LINE")
         if [[ "$FILE" =~ ^"$PREFIX" ]]; then
             printf '%s\n' "${FILE#$PREFIX}"
         fi

commit 6d48b237fb48678be0b4bde80282f259923578f9

write_makefiles: fix sbin/*:rootfs files ending in proprietary/rootfs/bin/
* The bug was introduced in commit 72765e4cc
  "write_makefiles: fix paths in BUILD_PREBUILT with destination"
* The commit mistakenly interpreted the description of the prefix_match
  function as returning strictly a list of files, so it stripped away
  the target_args.
* In reality, the write_packages function depends on prefix_match not
  stripping away the target_args. The 'if [ "$ARGS" = "rootfs" ];'
  failed, as a result, causing the sbin files to fall under a different
  code path.
* The fix is to restore target_args to the list returned by prefix_match,
  and also to explicitly document the function header that it should not
  remove target_args.

Change-Id: I4b0835cd55b01a82cc6ab6bdcdbeb5daf0adc712
Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

build/tools/extract_utils.sh
@@ -146,16 +146,20 @@ function target_args() {
 # Internal function which loops thru the packages list and returns a new
 # list containing the matched files with the prefix stripped away.
 #
+# Function does not strip the target_args of files,
+# if present (such as :rootfs).
+#
 function prefix_match() {
     local PREFIX="$1"
     for LINE in "${PRODUCT_PACKAGES_LIST[@]}"; do
-        if ! [[ "$LINE" =~ "rootfs" ]]; then
-            local FILE=$(target_file "$LINE")
-        else
-            local FILE="$LINE"
-        fi
+        local FILE=$(target_file "$LINE")
+        local ARGS=$(target_args "$LINE")
         if [[ "$FILE" =~ ^"$PREFIX" ]]; then
-            printf '%s\n' "${FILE#$PREFIX}"
+            if [ -z "$ARGS" ]; then
+                printf '%s\n'    "${FILE#$PREFIX}"
+            else
+                printf '%s:%s\n' "${FILE#$PREFIX}" "${ARGS}"
+            fi
         fi
     done
 }