2ef838206b250c2b0e056e1c8d4f6bc7b213532a
Vladimir Oltean <olteanv@gmail.com>
2018-05-25 01:55:22 +0300

    tasks: define BOARD_SYSTEM_EXTRA_FOLDERS and BOARD_SYSTEM_EXTRA_SYMLINKS
    * This mirrors the existence of BOARD_ROOT_EXTRA_FOLDERS and
      BOARD_ROOT_EXTRA_SYMLINKS, both parsed and created in
      system/core/rootdir/Android.mk.
    * A simple $(find . -name '*.mk' | xargs -r grep "SYMLINK") will reveal
      some atrocities in device makefiles to pin these down just right.
      Maybe some system-level support would help them clean up a bit.
    
    Change-Id: If59b2c0c2da290ff376b3eadd58bdded4c3c998d
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

---
+++ build/tasks/target_symlinks.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Very similar in scope to the ramdisk board-defined
+# symlinks created in system/core/rootdir/Android.mk.
+
+LOCAL_POST_INSTALL_CMD := mkdir -p $(BOARD_SYSTEM_EXTRA_FOLDERS)
+
+ifdef BOARD_SYSTEM_EXTRA_SYMLINKS
+# BOARD_SYSTEM_EXTRA_SYMLINKS is a list of <target>:<link_name>.
+  LOCAL_POST_INSTALL_CMD += $(foreach s, $(BOARD_SYSTEM_EXTRA_SYMLINKS), \
+    $(eval p := $(subst :,$(space),$(s)))                                \
+    $(eval system := $(PRODUCT_OUT)/$(TARGET_COPY_OUT_SYSTEM))           \
+    ; mkdir -p $(dir $(system)/$(word 2,$(p)))                           \
+    ; rm -rf $(system)/$(word 2,$(p))                                    \
+    ; ln -sf $(word 1,$(p)) $(system)/$(word 2,$(p)))
+endif
+
+ALL_DEFAULT_INSTALLED_MODULES += $(LOCAL_POST_INSTALL_CMD)
+
