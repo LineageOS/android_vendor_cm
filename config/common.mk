a61a70469b5b8d95f7e423c22a9d54f62cb80449
Nolen Johnson <johnsonnolen@gmail.com>
2024-10-03 13:08:44 -0400

    cm: config: common: Enable ADB Root by default
    Change-Id: Ia086108c96f180fbb3bc6ba0c2f7a3f8eee76d01

--- config/common.mk
+++ config/common.mk
@@ -45,6 +45,8 @@ ifneq ($(TARGET_BUILD_VARIANT),eng)
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
+ADDITIONAL_DEFAULT_PROPERTIES += cm.service.adb.root=1
+
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
     vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
